212943992d538f1a306e38bf8b381362f8cf9962
Suparna Nikam <nikam.suparna.s@gmail.com>
2026-03-07 14:25:08 +0530

    Assignment

---
+++ javascript-training/hello-world.js
@@ -0,0 +1,19 @@
+console.log("Hellowold1");
+console.log("Helllo World2");
+console.log("Helllo World3");
+console.log("Hellowold4");
+sumof2numbers(6,1)
+console.log("Helllo World5");
+console.log("Helllo World6");
+console.log("Hellowold7");
+console.log("Helllo World9");
+console.log("Helllo World8");
+
+
+function sumof2numbers(a,b)
+{
+    c = a+b;
+    console.log("Sum = "+ c)
+}
+const ss = 10;
+ss = 20;
